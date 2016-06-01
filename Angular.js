21efb2e6cf47b7b62e05d576d569d882a4f837f5
Margarita <margaritasadov@gmail.com>
2016-06-02 02:34:06 +0300

    Fixed few issues

--- Angular.js
+++ Angular.js
@@ -2,28 +2,30 @@
  * Created by Margarita on 01.06.2016.
  */
 
-app = angular.module('FormTable',[]);
-app.controller("myCtrl",['$scope', '$filter', function($scope, $filter){
+app = angular.module('FormTable', []);
+app.controller("myCtrl", ['$scope', '$filter', function ($scope, $filter) {
     $scope.people = [];
-    $scope.addData = function() {
+    $scope.gender = "Male";
+    $scope.addData = function () {
         var person = {
-            ngFirstName:$scope.firstName,
+            ngFirstName: $scope.firstName,
             ngLastName: $scope.lastName,
             ngPhone: $scope.phone,
-            ngGender:$scope.gender,
-            ngAge:$scope.age,
+            ngGender: $scope.gender,
+            ngAge: $scope.age
         };
         $scope.people.push(person);
         $scope.order('ngFirstName');
+        $scope.gender = "Male";
         clearUpFields_void();
     };
 
-    $scope.removeData = function(curPers) {
-        $scope.people.splice($scope.people.indexOf(curPers),1);
+    $scope.removeData = function (curPers) {
+        $scope.people.splice($scope.people.indexOf(curPers), 1);
     };
 
     var orderBy = $filter('orderBy');
-    $scope.order = function(predicate) {
+    $scope.order = function (predicate) {
         $scope.predicate = predicate;
         $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
         $scope.people = orderBy($scope.people, predicate, $scope.reverse);
--- Generic.js
+++ Generic.js
@@ -3,43 +3,50 @@
  */
 
 var validCreds = {
-    firstName: function(element){
-        return element.value!="";
+    firstName: function (element) {
+        return element.value != "";
     },
-    lastName: function(element){
-        return element.value!="";
+    lastName: function (element) {
+        return element.value != "";
     },
-    phone: function(element){
-        var rexPhoneMask =/^\+\d{11,13}/;
-        var result = (element.value!="" && rexPhoneMask.test(element.value))
+    phone: function (element) {
+        var rexPhoneMask = /^\+\d{11,13}/;
+        var result = (element.value != "" && rexPhoneMask.test(element.value));
         return result;
     },
-    age: function(element){
-        var result = (element.value!="" && parseFloat(element.value) > 0)
+    age: function (element) {
+        var result = (element.value != "" && parseFloat(element.value) > 0);
         return result;
     }
 };
 
-function checkField_void(element){
-    if(!validCreds[element.name](element)){
-        element.parentElement.setAttribute("class","form-group has-error")
-    }else{
-        element.parentElement.setAttribute("class","form-group")
+function checkField_void(element) {
+    if (!validCreds[element.name](element)) {
+        element.parentElement.setAttribute("class", "form-group has-error")
+    } else {
+        element.parentElement.setAttribute("class", "form-group")
     }
-    if(document.getElementsByClassName("form-group has-error").length >0){
+    var allFilled = true;
+    var inputNodes = document.getElementsByTagName("input");
+    for (var i = 0; i < inputNodes.length; i++) {
+        if ((inputNodes[i].type == "text" || inputNodes[i].type == "number")&& inputNodes[i].value == ""){
+                allFilled = false;
+        }
+    }
+    if (document.getElementsByClassName("form-group has-error").length > 0 || !allFilled) {
         document.getElementById("putInTable").disabled = true;
-    }else{
+    } else {
         document.getElementById("putInTable").disabled = false;
     }
 
 }
 
-function clearUpFields_void(){
+function clearUpFields_void() {
     var inputNodes = document.getElementsByTagName("input");
-    for(var i=0; i<inputNodes.length; i++){
-        if(inputNodes[i].type=="text" || inputNodes[i].type=="number"){
-            inputNodes[i].value="";
+    for (var i = 0; i < inputNodes.length; i++) {
+        if (inputNodes[i].type == "text" || inputNodes[i].type == "number") {
+            inputNodes[i].value = "";
         }
     }
-
+    document.getElementById("putInTable").disabled = false;
 }
